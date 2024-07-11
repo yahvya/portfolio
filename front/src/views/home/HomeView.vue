b194c631b31a9d60097d9fb732f6c50e921a6d30
Bathily Yahaya <developpeur.yb@gmail.com>
2024-07-11 12:53:06 +0200

    définition du composant barre de navigation définition de la page d'accueil ajout du support sass

---
+++ front/src/views/home/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+    import ClassicNavbar from "../../components/classic-navbar/ClassicNavbar.vue"
+
+    export default{
+        components: {
+            ClassicNavbar: ClassicNavbar
+        }
+    };
+</script>
+
+<template>
+    <ClassicNavbar/>
+</template>
+
+<style src="./HomeView.scss" scoped lang="scss">
+</style>
